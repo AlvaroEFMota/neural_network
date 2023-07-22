c8b6df1390433c9c27911ff7d64930446340de4a
Álvaro E. F. Mota <alvaroefmota@gmail.com>
2023-07-22 10:47:26 -0300

    Formatting the code

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use csv::Reader;
 use ndarray::{array, Array, Dim};
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
+use rand::prelude::*;
 use std::error::Error;
 use std::ops::Add;
-use rand::prelude::*;
 
 const EULER: f64 = std::f64::consts::E;
 const RANDOM_RANGE: f64 = 0.5;
@@ -14,9 +14,8 @@ const EPOCHS: i64 = 100;
 type Matrix1D = Array<f64, Dim<[usize; 1]>>;
 type Matrix2D = Array<f64, Dim<[usize; 2]>>;
 
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut all_data: Vec<(Matrix1D , Matrix1D)> = vec![]; 
+    let mut all_data: Vec<(Matrix1D, Matrix1D)> = vec![];
 
     // Proof of concept 1, inputs
     // all_data.push((array![0., 1.], array![0.]));
@@ -35,13 +34,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     // all_data.push((array![0.1, 0.5], array![0., 0.]));
     // let training_data: &[(Matrix1D , Matrix1D)] = &all_data[..];
     // let validation_data: &[(Matrix1D , Matrix1D)] = &all_data[..];
-    // let network = vec![2, 4, 2];    
+    // let network = vec![2, 4, 2];
 
     // Banknotes inputs
     let mut rdr = Reader::from_path("data/banknotes.csv").unwrap();
     for result in rdr.records() {
         let record = result?;
-        let input: Matrix1D  = array![
+        let input: Matrix1D = array![
             record[0].parse::<f64>()?,
             record[1].parse::<f64>()?,
             record[2].parse::<f64>()?,
@@ -51,7 +50,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         all_data.push((input, desired_output));
     }
     shuffle(&mut all_data[..300]);
-    let training_data: &[(Matrix1D , Matrix1D)] = &all_data[..300];
+    let training_data: &[(Matrix1D, Matrix1D)] = &all_data[..300];
     let validation_data: &[(Matrix1D, Matrix1D)] = &all_data[300..];
     let network = vec![4, 4, 4, 1];
 
@@ -65,15 +64,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             (network[i], network[i + 1]),
             Uniform::new(-RANDOM_RANGE, RANDOM_RANGE),
         );
-        let b: Matrix1D  =
-            Array::zeros(network[i + 1]);
+        let b: Matrix1D = Array::zeros(network[i + 1]);
         weights.push(w);
         biases.push(b);
     }
     for _ in 0..EPOCHS {
         let mut weight_gradients: Vec<Matrix2D> = vec![];
         let mut bias_gradients: Vec<Matrix1D> = vec![];
- 
+
         for weight in weights.iter() {
             let _x = weight.raw_dim();
             let weight_gradient = Array::zeros(weight.raw_dim());
@@ -99,18 +97,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("{:?}", error);
         error = Array::zeros(network.len());
     }
-    
+
     let mut hit_count = 0;
     let error_margin = 0.03;
     for (input, output) in validation_data {
         let mut layers: Vec<Matrix1D> = vec![];
         let mut zs: Vec<Matrix1D> = vec![];
-        let neural_network_output = forward_propagation(input, &weights, &biases, &mut layers, &mut zs);
-        println!(
-            "Forward propagation result -> {:?}",
-            neural_network_output
-        );
-        
+        let neural_network_output =
+            forward_propagation(input, &weights, &biases, &mut layers, &mut zs);
+        println!("Forward propagation result -> {:?}", neural_network_output);
+
         let mut hit = true;
         for (i, elem) in neural_network_output.iter().enumerate() {
             if !(elem + error_margin > output[i] && elem - error_margin < output[i]) {
@@ -125,7 +121,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         layers.clear();
     }
     println!("Total hits = {hit_count}");
-    println!("hits percent = {}%", (hit_count as f32 / validation_data.len() as f32) * 100.0);
+    println!(
+        "hits percent = {}%",
+        (hit_count as f32 / validation_data.len() as f32) * 100.0
+    );
     Ok(())
 }
 
@@ -157,7 +156,7 @@ fn back_propagation(
     network_desired_output: &Matrix1D,
     error: &mut Matrix1D,
 ) {
-    let mut delta = &layers[layers.len()-1] - network_desired_output;
+    let mut delta = &layers[layers.len() - 1] - network_desired_output;
 
     for (i, _layer) in layers.iter().enumerate().rev() {
         if i > 0 {
@@ -172,30 +171,28 @@ fn back_propagation(
             }
             error[i] = sum;
 
-            let mut weight_gradient_part: Matrix2D =
-                Array::zeros(weight.raw_dim());
-            let mut bias_gradient_part: Matrix1D =
-                Array::zeros(biases[i - 1].raw_dim());
+            let mut weight_gradient_part: Matrix2D = Array::zeros(weight.raw_dim());
+            let mut bias_gradient_part: Matrix1D = Array::zeros(biases[i - 1].raw_dim());
 
-                for (j, mut matrix_row) in weight_gradient_part.outer_iter_mut().enumerate() {
-                    for (k, matrix_element) in matrix_row.iter_mut().enumerate() {
-                        *matrix_element +=
-                            previous_layer[j] * LEARNING_RATE * delta[k] * sigmoid_derivative(z[k]);
-                    }
+            for (j, mut matrix_row) in weight_gradient_part.outer_iter_mut().enumerate() {
+                for (k, matrix_element) in matrix_row.iter_mut().enumerate() {
+                    *matrix_element +=
+                        previous_layer[j] * LEARNING_RATE * delta[k] * sigmoid_derivative(z[k]);
                 }
-                for (j, matrix_element) in bias_gradient_part.iter_mut().enumerate() {
-                    *matrix_element += LEARNING_RATE * delta[j] * sigmoid_derivative(z[j]);
-                }
+            }
+            for (j, matrix_element) in bias_gradient_part.iter_mut().enumerate() {
+                *matrix_element += LEARNING_RATE * delta[j] * sigmoid_derivative(z[j]);
+            }
 
             weight -= &weight_gradient_part;
 
-            let mut next_iteration_delta: Matrix1D =
-                Array::zeros(previous_layer.raw_dim());
+            let mut next_iteration_delta: Matrix1D = Array::zeros(previous_layer.raw_dim());
 
             for (j, matrix_element) in next_iteration_delta.iter_mut().enumerate() {
                 let mut sum = 0.0;
                 for column in 0..weight_gradient_part.ncols() {
-                    sum += weight[[j, column]] * sigmoid_derivative(previous_layer[j]) * delta[column];
+                    sum +=
+                        weight[[j, column]] * sigmoid_derivative(previous_layer[j]) * delta[column];
                 }
                 *matrix_element += sum;
             }
@@ -218,7 +215,7 @@ fn sigmoid_derivative(x: f64) -> f64 {
 #[allow(dead_code)]
 fn shuffle<T>(data: &mut [T]) {
     let mut rng = rand::thread_rng();
-    for i in 0..data.len()-1 {
-        data.swap(i, rng.gen::<usize>()%data.len());
+    for i in 0..data.len() - 1 {
+        data.swap(i, rng.gen::<usize>() % data.len());
     }
 }
